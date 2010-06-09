40773ef21a82e4fae4972294ee9ecdcb88719035
testxxxxxxPicasa <testxxxxxxPicasa@88ad2618-cff9-683c-2cdb-10b533243f04>
2010-06-09 09:48:22 +0000

    v1.0.1 建立SVN

---
+++ trunk/WebContent/example/node.php
@@ -0,0 +1,21 @@
+[
+	{ name:"手机", open:false,
+		nodes: [
+			{ name:"诺基亚", isParent:true},
+			{ name:"三星"},
+			{ name:"索爱"},
+			{ name:"多普达"}
+		]},
+	{ name:"电脑", open:true,
+		nodes: [
+			{ name:"硬件"},
+			{ name:"整机", isParent:true},
+			{ name:"网络"}
+		]},
+	{ name:"家电", open:false,
+		nodes: [
+			{ name:"电视"},
+			{ name:"冰箱"},
+			{ name:"空调", isParent:true}
+		]}
+]
