6b782618355ad05edbe2eeeb85e84d2f7a8e3f8d
testxxxxxxPicasa <testxxxxxxPicasa@88ad2618-cff9-683c-2cdb-10b533243f04>
2011-12-23 03:23:55 +0000

    【修改】简单数据模式下，如果 id 与 pId 的值相同会导致该节点无法正常加载的 bug

--- zTreeAPI/WebContent/js/jquery.ztree.core-3.0.js
+++ zTreeAPI/WebContent/js/jquery.ztree.core-3.0.js
@@ -551,7 +551,7 @@
 					tmpMap[sNodes[i][key]] = sNodes[i];
 				}
 				for (i=0, l=sNodes.length; i<l; i++) {
-					if (tmpMap[sNodes[i][parentKey]]) {
+					if (tmpMap[sNodes[i][parentKey]] && sNodes[i][key] != sNodes[i][parentKey]) {
 						if (!tmpMap[sNodes[i][parentKey]][childKey])
 							tmpMap[sNodes[i][parentKey]][childKey] = [];
 						tmpMap[sNodes[i][parentKey]][childKey].push(sNodes[i]);
