5bc33c44905d598939ba9d4b329203979b45da22
testxxxxxxPicasa <testxxxxxxPicasa@88ad2618-cff9-683c-2cdb-10b533243f04>
2012-11-19 08:50:12 +0000

    v3.5

--- zTreeAPI/WebContent/js/jquery.ztree.excheck-3.5.js
+++ zTreeAPI/WebContent/js/jquery.ztree.excheck-3.5.js
@@ -1,5 +1,5 @@
 /*
- * JQuery zTree excheck 3.4
+ * JQuery zTree excheck 3.5
  * http://zTree.me/
  *
  * Copyright (c) 2010 Hunter.z
@@ -8,7 +8,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  *
  * email: hunter.z@263.net
- * Date: 2012-09-03
+ * Date: 2012-11-20
  */
 (function($){
 	//default consts of excheck
