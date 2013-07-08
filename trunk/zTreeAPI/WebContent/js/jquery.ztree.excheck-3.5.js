ee4f9ce208306d8985450d5cb01d573626b322ea
testxxxxxxPicasa <testxxxxxxPicasa@88ad2618-cff9-683c-2cdb-10b533243f04>
2013-07-08 03:15:47 +0000

    * 【修改】 编辑状态， beforeRename 回调 return false 时，提示信息导致 input 失去焦点后， 当树再次得到焦点时，让 input 自动获取焦点

--- trunk/zTreeAPI/WebContent/js/jquery.ztree.excheck-3.5.js
+++ trunk/zTreeAPI/WebContent/js/jquery.ztree.excheck-3.5.js
@@ -8,7 +8,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  *
  * email: hunter.z@263.net
- * Date: 2013-07-05
+ * Date: 2013-07-08
  */
 (function($){
 	//default consts of excheck
--- trunk/zTreeAPI/WebContent/js/jquery.ztree.exedit-3.5.js
+++ trunk/zTreeAPI/WebContent/js/jquery.ztree.exedit-3.5.js
@@ -8,7 +8,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  *
  * email: hunter.z@263.net
- * Date: 2013-07-05
+ * Date: 2013-07-08
  */
 (function($){
 	//default consts of exedit
@@ -1176,6 +1176,10 @@
 		var root = data.getRoot(setting);
 		if (e && (tools.eqs(e.type, "mouseover") || tools.eqs(e.type, "mouseout") || tools.eqs(e.type, "mousedown") || tools.eqs(e.type, "mouseup"))) {
 			return true;
+		}
+		if (root.curEditNode) {
+			view.editNodeBlur = false;
+			root.curEditInput.focus();
 		}
 		return (!root.curEditNode) && (_uCanDo ? _uCanDo.apply(view, arguments) : true);
 	}
